aa570990cfa6f9d54b2f9c41beb3534efa7df41a
David Chung <chungs_kr@hotmail.com>
2015-01-31 15:43:01 -0800

    ICSWebCrawler Calss

---
+++ src/ir/assignments/three/ICSWebCrawler.java
@@ -0,0 +1,10 @@
+package ir.assignments.three;
+
+import edu.uci.ics.crawler4j.crawler.WebCrawler;
+
+/**
+ * Created by dch on 1/31/15.
+ */
+public class ICSWebCrawler implements WebCrawler {
+
+}
